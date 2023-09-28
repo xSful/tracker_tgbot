addbdaa4470efa2cb0ffb87bfdd9e5c2a7d06dbe
xSful <87297897+xSful@users.noreply.github.com>
2023-09-28 23:04:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 API_TOKEN = '6542591447:AAFVK10-b_dB5ofGjg_bWfjgJFIIsRy6I3s'
-#5290917625:AAHW2yP5zNajYE1MDVYUwO8c6iA8y2cYi14
 con = sqlite3.connect('db.db')
 cursor = con.cursor()
 
